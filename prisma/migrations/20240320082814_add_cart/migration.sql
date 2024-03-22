0ebec7aaeecbbe65b0c8647b29d5bce9ea18abf1
aman7840 <amanwadhwaniya159@gmail.com>
2024-03-22 13:14:16 +0530

    fix: Deleted all migrations

--- prisma/migrations/20240320082814_add_cart/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "add_to_cart" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "wish_list" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240320091859_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Discount" ALTER COLUMN "discountPrice" SET DATA TYPE TEXT,
-ALTER COLUMN "discountPerc" SET DATA TYPE TEXT;
